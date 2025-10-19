3efd897d62e66239841ea97d375f027619b127f3
Michał Kołacz <michal.kolacz45@gmail.com>
2025-10-19 19:52:45 +0200

    refactor: remove unused functions

--- src/services/auth.server.ts
+++ src/services/auth.server.ts
@@ -1,4 +1,3 @@
-import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
 import { prisma, type User } from '#app/lib/db.server'
@@ -10,33 +9,6 @@ export interface AuthenticatedUser {
 	role: User['role']
 	createdAt: Date
 	updatedAt: Date
-}
-
-export async function verifyCredentials(username: string, password: string) {
-	const user = await prisma.user.findUnique({
-		where: { username },
-		include: {
-			password: true,
-		},
-	})
-
-	if (!user || !user.password) return null
-
-	const isPasswordValid = await bcrypt.compare(password, user.password.hash)
-
-	if (!isPasswordValid) return null
-
-	return {
-		id: user.id,
-		username: user.username,
-		role: user.role,
-		createdAt: user.createdAt,
-		updatedAt: user.updatedAt,
-	}
-}
-
-export async function hashPassword(plainTextPassword: string): Promise<string> {
-	return bcrypt.hash(plainTextPassword, 10)
 }
 
 export async function getCurrentUser(): Promise<AuthenticatedUser | null> {
@@ -68,34 +40,5 @@ export async function getCurrentUser(): Promise<AuthenticatedUser | null> {
 		role: user.role,
 		createdAt: user.createdAt,
 		updatedAt: user.updatedAt,
-	}
-}
-
-export async function registerUser(
-	username: string,
-	password: string,
-): Promise<AuthenticatedUser> {
-	const hashedPassword = await hashPassword(password)
-
-	try {
-		const user = await prisma.user.create({
-			data: {
-				username,
-				role: 'user',
-				password: {
-					create: { hash: hashedPassword },
-				},
-			},
-		})
-
-		return {
-			id: user.id,
-			username: user.username,
-			role: user.role,
-			createdAt: user.createdAt,
-			updatedAt: user.updatedAt,
-		}
-	} catch (error) {
-		throw error
 	}
 }
