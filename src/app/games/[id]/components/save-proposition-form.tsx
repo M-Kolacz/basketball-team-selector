b7f6a2492101383e7d39aea1ca12a538603f003e
Michał Kołacz <michal.kolacz45@gmail.com>
2025-11-05 20:33:39 +0100

    fix: add missing constraint to SavePropositionForm

--- src/app/games/[id]/components/save-proposition-form.tsx
+++ src/app/games/[id]/components/save-proposition-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { getFormProps, getInputProps, useForm } from '@conform-to/react'
-import { parseWithZod } from '@conform-to/zod'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
 import { Fragment, useActionState } from 'react'
 import { Button } from '#app/components/ui/button'
 import { updatePropositionTeams } from '#app/lib/actions/game-sessions'
@@ -20,6 +20,7 @@ export const SavePropositionForm = ({
 		undefined,
 	)
 	const [form, fields] = useForm({
+		constraint: getZodConstraint(SavePropositionSchema),
 		lastResult: lastResult?.result,
 		defaultValue: {
 			updatedTeams: updatedTeams,
