dd66ec1d88858e3582d1816a3acdb3bacff6c405
Michał Kołacz <michal.kolacz45@gmail.com>
2025-11-16 21:06:35 +0100

    fix: update navigation path to statistics page in e2e tests

--- src/e2e/statistics.spec.ts
+++ src/e2e/statistics.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('User can see statistics table', async ({ page }) => {
-	await page.goto('/players/stats')
+	await page.goto('/statistics')
 
 	await expect(
 		page.getByRole('heading', { name: 'Player Statistics', level: 1 }),
