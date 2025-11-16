6185649e4d99228a0820b88d987c375ef11c2643
Michał Kołacz <michal.kolacz45@gmail.com>
2025-11-16 21:16:00 +0100

    refactor: update statistics table test to reflect new column names

--- src/e2e/statistics.spec.ts
+++ src/e2e/statistics.spec.ts
@@ -7,9 +7,9 @@ test('User can see statistics table', async ({ page }) => {
 		page.getByRole('heading', { name: 'Player Statistics', level: 1 }),
 	).toBeVisible()
 	await expect(page.getByRole('row', { name: 'Player Name' })).toBeVisible()
-	await expect(page.getByRole('row', { name: 'Total Games' })).toBeVisible()
-	await expect(page.getByRole('row', { name: 'Games Won' })).toBeVisible()
-	await expect(page.getByRole('row', { name: 'Win Ratio' })).toBeVisible()
+	await expect(
+		page.getByRole('row', { name: 'Number of the game sessions played' }),
+	).toBeVisible()
 	const playersRows = await page.getByRole('row').count()
 	expect(playersRows).toBeGreaterThan(2)
 })
