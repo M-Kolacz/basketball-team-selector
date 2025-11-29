6dcf568af8da73d2e7391e039554ca6f980df9ae
Michał Kołacz <michal.kolacz45@gmail.com>
2025-11-29 15:10:12 +0100

    feat: fix e2e tests

--- src/e2e/auth.spec.ts
+++ src/e2e/auth.spec.ts
@@ -2,43 +2,42 @@ import { test, expect } from '@playwright/test'
 
 test('User should be able to log in', async ({ page }) => {
 	await page.goto('/login')
+	const main = page.getByRole('main')
+	const header = page.getByRole('banner')
 
 	await page.getByRole('textbox', { name: 'Username' }).fill('kody')
 	await page.getByRole('textbox', { name: 'Password' }).fill('kodylovesyou')
+	await main.getByRole('button', { name: 'Login' }).click()
 
-	await page.getByRole('button', { name: 'Login' }).click()
-
-	await page.getByRole('button', { name: 'User menu' }).click()
-	await expect(page.getByText('Admin')).toBeVisible()
+	await expect(header.getByRole('button', { name: 'Logout' })).toBeVisible()
 })
 
 test('User should be able to log out', async ({ page }) => {
 	await page.goto('/login')
+	const main = page.getByRole('main')
+	const header = page.getByRole('banner')
 
-	await page.getByRole('textbox', { name: 'Username' }).fill('kody')
-	await page.getByRole('textbox', { name: 'Password' }).fill('kodylovesyou')
+	await main.getByRole('textbox', { name: 'Username' }).fill('kody')
+	await main.getByRole('textbox', { name: 'Password' }).fill('kodylovesyou')
+	await main.getByRole('button', { name: 'Login' }).click()
 
-	await page.getByRole('button', { name: 'Login' }).click()
-
-	await page.getByRole('button', { name: 'User menu' }).click()
-	await page.getByRole('button', { name: 'Logout' }).click()
-
-	await page.getByRole('button', { name: 'Login' }).click()
+	await header.getByRole('button', { name: 'Logout' }).click()
+	await expect(header.getByRole('button', { name: 'Login' })).toBeVisible()
 })
 
 test('User should be able to register', async ({ page }) => {
 	await page.goto('/register')
+	const main = page.getByRole('main')
+	const header = page.getByRole('banner')
 
-	await page.getByRole('textbox', { name: 'Username' }).fill('newUser')
-	await page
+	await main.getByRole('textbox', { name: 'Username' }).fill('newUser')
+	await main
 		.getByRole('textbox', { name: 'Password', exact: true })
 		.fill('testing123')
-	await page
+	await main
 		.getByRole('textbox', { name: 'Confirm Password' })
 		.fill('testing123')
+	await main.getByRole('button', { name: 'Create account' }).click()
 
-	await page.getByRole('button', { name: 'Create account' }).click()
-
-	await page.getByRole('button', { name: 'User menu' }).click()
-	await expect(page.getByText('User', { exact: true })).toBeVisible()
+	await expect(header.getByRole('button', { name: 'Logout' })).toBeVisible()
 })
